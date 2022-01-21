c67471ed0ce25d5ef06cab6d9b0aac798fb0faa0
Erlend Blomseth <erlend@elendig.no>
2022-01-21 10:49:45 +0100

    Add generated valve from fordypningsprosjekt

--- model_spawner/src/model_spawner.py
+++ model_spawner/src/model_spawner.py
@@ -1,13 +1,60 @@
 #!/usr/bin/env python3
 
 import rospy
+from gazebo_msgs.srv import SpawnModel
+from geometry_msgs.msg import Quaternion, Pose, Point
+from generate_valve import Valve
+from math import pi
 
-def add_model():
-    pass
+def add_valve():
+    rospy.loginfo("Waiting for service gazebo/spawn_sdf_model ...")
+    rospy.wait_for_service("gazebo/spawn_sdf_model")
+    
+    rospy.loginfo('Generating model ...')
+
+    model_path = "/home/catkin_ws/src/model_spawner/src/valve"
+    x_offset = 0
+    y_offset = 0
+    z_offset = 1
+    roll_offset = 0
+    pitch_offset = 0
+    yaw_offset = 0
+    pipe_radius = 0.2
+    pipe_length = 1
+    valve_pipe_radius = 0.2
+    valve_pipe_length = 0.5
+    valve_radius = 0.2
+
+    valve = Valve(
+        model_path = model_path,
+        x_offset = x_offset,
+        y_offset = y_offset,
+        z_offset = z_offset,
+        roll_offset = roll_offset,
+        pitch_offset = pitch_offset,
+        yaw_offset = yaw_offset,
+        pipe_radius = pipe_radius,
+        pipe_length = pipe_length,
+        valve_pipe_radius = valve_pipe_radius,
+        valve_pipe_length = valve_pipe_length,
+        valve_radius = valve_radius
+    )
+    valve.generate_sdf()
+
+    rospy.loginfo('Adding generated pipe ...')
+
+    with open('/home/catkin_ws/src/model_spawner/src/valve/model.sdf') as file:
+        model_xml = file.read()
+    
+    orientation = Quaternion(0,0,0,0)
+    pose = Pose(Point(x=0, y=0, z=0.5), orientation)
+
+    spawn_sdf_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+    spawn_sdf_model("model", model_xml, "", pose, "world")
 
 if __name__ == '__main__':
     try:
         rospy.init_node('model_spawner', anonymous=True)
-        add_model()
+        add_valve()
     except rospy.ROSInterruptException:
         pass
