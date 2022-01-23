8b57f2d4a5870cd89a5c18c102f9314df32eaf9c
Erlend Blomseth <erlend@elendig.no>
2022-01-23 16:11:14 +0100

    Subscribe to model states in mission planner node

--- mission_planner/src/mission_planner.py
+++ mission_planner/src/mission_planner.py
@@ -4,6 +4,7 @@ import rospy
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Quaternion, Pose, Point
 from tf.transformations import quaternion_from_euler
+from time import sleep
 
 def pose_from_position_and_orientation(x, y, z, a, b, c):
     q = quaternion_from_euler(a, b, c)
@@ -16,8 +17,22 @@ class MissionPlanner:
         self.points_of_interest = points_of_interest
     
     def plan(self):
+        self.subscribe_to_model_states()
+        
+        # Wait 5 seconds to load all model states
+        i = 0
+        while (i < 5):
+            sleep(1)
+            i += 1
+
         inspection_poses = self.find_inspection_poses()
         return inspection_poses
+    
+    def subscribe_to_model_states(self):
+        rospy.Subscriber("gazebo/model_states", ModelStates, self.store_model_states)
+    
+    def store_model_states(self, model_states):
+        self.model_states = model_states
 
     def find_inspection_poses(self):
         inspection_poses = [None] * len(self.points_of_interest)
