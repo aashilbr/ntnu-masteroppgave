2408a7db0634e6db0fcd68a916d372b844f26add
Erlend Blomseth <erlend@elendig.no>
2022-02-15 13:13:19 +0100

    Use huldra-smaller-walkway with pymesh test script

--- mission_planner/src/walkway.py
+++ mission_planner/src/walkway.py
@@ -2,7 +2,7 @@ import pymesh
 
 huldra_models_path = '../../resources/huldra-models/'
 
-mesh = pymesh.load_mesh(huldra_models_path + 'huldra-small-area-walkway/meshes/Huldra-small-area-walkway.obj')
+mesh = pymesh.load_mesh(huldra_models_path + 'huldra-smaller-walkway/meshes/huldra-smaller-walkway.obj')
 
 mesh.add_attribute('face_index')
 print(mesh.get_face_attribute('face_index'))
