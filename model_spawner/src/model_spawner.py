3d8a472c1f079992354f45239f68029c07621c09
Erlend Blomseth <erlend@elendig.no>
2022-02-15 13:01:36 +0100

    Add huldra-smaller and huldra-smaller-walkway models, together with spawner methods

--- model_spawner/src/model_spawner.py
+++ model_spawner/src/model_spawner.py
@@ -88,6 +88,32 @@ def add_huldra_small_area_walkway():
     spawn_sdf_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
     spawn_sdf_model("huldra-small-area-walkway", model_xml, "", pose, "world")
 
+def add_huldra_smaller():
+    rospy.loginfo('Adding Huldra-smaller...')
+
+    with open('/home/catkin_ws/src/model_spawner/src/huldra-models/huldra-smaller/model.sdf') as file:
+        model_xml = file.read()
+    
+    q = quaternion_from_euler(pi/2, 0, 0)
+    orientation = Quaternion(q[0], q[1], q[2], q[3])
+    pose = Pose(Point(x=116, y=287, z=-27), orientation)
+
+    spawn_sdf_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+    spawn_sdf_model("huldra-smaller", model_xml, "", pose, "world")
+
+def add_huldra_smaller_walkway():
+    rospy.loginfo('Adding Huldra-smaller-walkway...')
+
+    with open('/home/catkin_ws/src/model_spawner/src/huldra-models/huldra-smaller-walkway/model.sdf') as file:
+        model_xml = file.read()
+    
+    q = quaternion_from_euler(pi/2, 0, 0)
+    orientation = Quaternion(q[0], q[1], q[2], q[3])
+    pose = Pose(Point(x=116, y=287, z=-27), orientation)
+
+    spawn_sdf_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+    spawn_sdf_model("huldra-smaller-walkway", model_xml, "", pose, "world")
+
 if __name__ == '__main__':
     try:
         rospy.init_node('model_spawner', anonymous=True)
@@ -96,6 +122,8 @@ if __name__ == '__main__':
         #add_valve()
         #add_floor()
         #add_huldra_small_area()
-        add_huldra_walkway()
+        #add_huldra_small_area_walkway()
+        #add_huldra_smaller()
+        add_huldra_smaller_walkway()
     except rospy.ROSInterruptException:
         pass
