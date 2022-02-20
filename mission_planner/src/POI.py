5bbe95dffb12bdc54d5dfb2a169ede1c7130a2d7
Erlend Blomseth <erlend@elendig.no>
2022-02-20 17:13:58 +0100

    Change orientation to direction. A POI will most probably have one side/face at which it is possible to inspect. That face will need a position and a direction. The direction will be a vector pointing out from the face

--- mission_planner/src/POI.py
+++ mission_planner/src/POI.py
@@ -1,5 +1,5 @@
 class POI():
-    def __init__(self, identifier, position = [0, 0, 0], orientation = [0, 0, 0]):
+    def __init__(self, identifier, position = [0, 0, 0], direction = [0, 0, 0]):
         self.identifier = identifier
         self.position = position
-        self.orientation = orientation
+        self.direction = direction
