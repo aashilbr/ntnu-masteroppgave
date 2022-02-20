5db693ae07df69d4bb32a091ee6676c06968bc4b
Erlend Blomseth <erlend@elendig.no>
2022-02-20 19:21:58 +0100

    Cleanup in Mission Planner

--- mission_planner/src/mission_planner.py
+++ mission_planner/src/mission_planner.py
@@ -25,6 +25,8 @@ class MissionPlanner:
 
     def find_inspection_pose(self, poi: POI):
 
+        # TODO: Redo all of this with higher resolution, if we find any interesting parts of the walkway
+
         possible_inspection_points = self.wp.get_points_along_walkway_with_resolution(0)
 
         possible_inspection_points_scores = [0] * len(possible_inspection_points)
@@ -32,37 +34,39 @@ class MissionPlanner:
             possible_inspection_point = possible_inspection_points[i]
             score = 0
 
-            will_robot_crash = self.will_robot_crash_at_position(possible_inspection_point)
             distance_to_poi = self.get_distance_between_points(possible_inspection_point, poi.position)
+            will_inspector_crash = self.will_inspector_crash_at_position(possible_inspection_point)
             are_there_obstacles = self.are_there_obstacles_between(possible_inspection_point, poi.position)
-            is_inspection_possible = self.is_inspection_possible_from_point(poi, possible_inspection_point)
-
+            is_poi_face_against_inspection_point = self.is_poi_face_against_inspection_point(poi, possible_inspection_point)
+            score_from_image_analysis = self.get_score_from_image_analysis(possible_inspection_point, poi.position)
             # TODO: Use image processing to find if the inspection is useful
 
-            # TODO: Make a better score function
-            score = distance_to_poi
+            score = distance_to_poi # TODO: Find a better score function
             possible_inspection_points_scores[i] = score
 
         possible_inspection_points_sorted = [x for _, x in sorted(zip(possible_inspection_points_scores, possible_inspection_points), key=lambda pair: pair[0])]
         inspection_pose = possible_inspection_points_sorted[0]
         return inspection_pose
     
-    def will_robot_crash_at_position(self, position):
-        # TODO: Check if robot crashes at the given point
-        return False # Dummy return value
-    
     def get_distance_between_points(self, p1, p2):
         distance = sqrt( (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2 )
         return distance
+    
+    def will_inspector_crash_at_position(self, position):
+        # TODO: Check if robot crashes at the given point. E.g. are there anything placed on the walkway?
+        return False # Dummy return value
     
     def are_there_obstacles_between(self, p1, p2):
         # TODO: Find obstacles with raycasting
         return False # Dummy return value
     
-    def is_inspection_possible_from_point(self, poi: POI, point):
-        # TODO: Check if we can inspect the valve, using the discovered valve inspection direction (poi.direction)
+    def is_poi_face_against_inspection_point(self, poi: POI, point):
+        # TODO: Check if we can inspect the valve, using the discovered valve inspection direction (poi.direction). Need to implement poi.direction first
         return True # Dummy return value
 
+    def get_score_from_image_analysis(self, inspection_point, poi_position):
+        # Use camera plugin to take an image of POI from inspection position, apply image analysis and get a score.
+        return 0 # Dummy return value
 
 if __name__ == '__main__':
     rospy.init_node('mission_planner', anonymous=True)
@@ -75,6 +79,8 @@ if __name__ == '__main__':
         inspection_poses = mission_planner.find_inspection_poses()
         print('Inspection poses')
         print(inspection_poses)
+
+        # TODO: Send the proposed inspection poses to an Inspector node
 
         while True:
             sleep(1)
