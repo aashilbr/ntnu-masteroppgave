373dec46001d569ef2c62d17e57bbcd5afb73c6d
Erlend Blomseth <erlend@elendig.no>
2022-02-20 17:01:36 +0100

    Make a POI (point of interest) class for holding POI information: identifier, position, and possibly orientation

---
+++ mission_planner/src/POI.py
@@ -0,0 +1,5 @@
+class POI():
+    def __init__(self, identifier, position = [0, 0, 0], orientation = [0, 0, 0]):
+        self.identifier = identifier
+        self.position = position
+        self.orientation = orientation
