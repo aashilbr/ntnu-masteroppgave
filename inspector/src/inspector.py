0df810731d128ef7a61ee2face3e27562bf6e1bc
Erlend Blomseth <erlend@elendig.no>
2022-02-22 13:35:51 +0100

    Add inspector node

---
+++ inspector/src/inspector.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rospy
+from gazebo_msgs.srv import SpawnModel
+from geometry_msgs.msg import Quaternion, Pose, Point
+from tf.transformations import quaternion_from_euler
+
+def add_model():
+    rospy.loginfo("Waiting for service gazebo/spawn_sdf_model ...")
+    rospy.wait_for_service("gazebo/spawn_sdf_model")
+
+    rospy.loginfo('Adding Inspector Robot model (this can take some time - camera model will be downloaded)...')
+    spawn_sdf_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+    with open('/home/catkin_ws/src/inspector/src/inspector_robot/inspector_robot.sdf') as file:
+        inspector_robot_xml = file.read()
+    
+    q = quaternion_from_euler(0, 0, 0)
+    orientation = Quaternion(q[0], q[1], q[2], q[3])
+    pose = Pose(Point(x=-2, y=-2, z=0), orientation)
+
+    spawn_sdf_model("inspector_robot", inspector_robot_xml, "", pose, "world")
+
+    rospy.loginfo("Inspector Robot model added.")
+
+if __name__ == '__main__':
+    rospy.init_node('inspector', anonymous=True)
+    try:
+        add_model()
+    except rospy.ROSInterruptException:
+        pass
