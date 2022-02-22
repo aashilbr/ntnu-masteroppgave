4ac131934cb757f4bafd0766deaa0a6f01f5d707
Erlend Blomseth <erlend@elendig.no>
2022-02-22 15:10:32 +0100

    Move robot to inspection pose in Inspector node

--- inspector/src/inspector.py
+++ inspector/src/inspector.py
@@ -3,6 +3,7 @@
 import rospy
 from gazebo_msgs.srv import SpawnModel
 from geometry_msgs.msg import Quaternion, Pose, Point, PoseArray
+from gazebo_msgs.msg import ModelState, ModelStates
 from tf.transformations import quaternion_from_euler
 from time import sleep
 
@@ -27,32 +28,74 @@ class Inspector():
     def __init__(self):
         self._poi_poses = None
         self._inspection_poses = None
+        self._robot_state = None
 
         rospy.loginfo('Listening for POI poses and inspection poses...')
         rospy.Subscriber("inspector/poi_poses", PoseArray, self._store_poi_poses)
         rospy.Subscriber("inspector/inspection_poses", PoseArray, self._store_inspection_poses)
+        rospy.Subscriber("gazebo/model_states", ModelStates, self._store_robot_state)
     
     def _store_poi_poses(self, poses_array: PoseArray):
         self._poi_poses = poses_array.poses
     
     def _store_inspection_poses(self, poses_array: PoseArray):
         self._inspection_poses = poses_array.poses
+    
+    def _store_robot_state(self, model_states: ModelStates):
+        if len(model_states.name) < 1:
+            return
+        
+        robot_state_index = False
+        for i in range(0, len(model_states.name)):
+            if model_states.name[i] == 'inspector_robot':
+                robot_state_index = i
+                break
+        if not robot_state_index:
+            return
+        
+        new_robot_state = ModelState(
+            model_states.name[robot_state_index],
+            model_states.pose[robot_state_index],
+            model_states.twist[robot_state_index],
+            'map'
+        )
+
+        if self._robot_state == None:
+            print('Storing Inspector Robot state.')
+            self._robot_state = new_robot_state
 
     def has_poses(self):
         if self._poi_poses != None and self._inspection_poses != None:
+            return True
+        return False
+    
+    def has_robot_state(self):
+        if self._robot_state != None:
             return True
         return False
 
     def inspect(self):
         print('Inspecting...')
         print(self._inspection_poses)
+        print()
+
+        print('Publishing new Inspector Robot state')
+        pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=10)
+        new_robot_state = self._robot_state
+        new_robot_state.pose = self._inspection_poses[0]
+        print(new_robot_state)
+
+        i = 0
+        while i <= 3:
+            pub.publish(new_robot_state)
+            sleep(1)
 
 if __name__ == '__main__':
     rospy.init_node('inspector', anonymous=True)
     try:
         add_model()
         inspector = Inspector()
-        while not inspector.has_poses():
+        while not inspector.has_poses() or not inspector.has_robot_state():
             sleep(1)
         inspector.inspect()
         
