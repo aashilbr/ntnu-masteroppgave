06f62aff4201bbbbb2be14e45b83f6706653f0c1
Erlend Blomseth <erlend@elendig.no>
2022-04-01 13:45:36 +0200

    Add custom empty world for better lighting

--- gazebo/entrypoint.sh
+++ gazebo/entrypoint.sh
@@ -4,6 +4,6 @@ echo 'source /opt/ros/noetic/setup.bash' >> ~/.bashrc
 source /opt/ros/noetic/setup.bash
 catkin build
 
-roslaunch --wait gazebo_ros empty_world.launch
+roslaunch --wait gazebo_ros empty_world.launch world_name:="/home/catkin_ws/src/gazebo/world/world.world"
 
 sleep 1d
